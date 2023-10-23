1a5ea91f6f3dc280399ab261077e59bf3e3e6a63
rams0003 <s12977.rakshith@gmail.com>
2023-10-23 19:48:47 +0530

    Scrollable view , Email and Phone Number Validation

--- app/src/main/java/com/example/studententryform/RegistrationForm.kt
+++ app/src/main/java/com/example/studententryform/RegistrationForm.kt
@@ -3,12 +3,17 @@ package com.example.studententryform
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import java.util.Calendar
 
 class RegistrationForm : AppCompatActivity() {
+
+    lateinit var submit: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_form)
@@ -29,5 +34,32 @@ class RegistrationForm : AppCompatActivity() {
             }, year, month, dayOfMonth)
             datePickerDialog.show()
         }
+
+        val submit = findViewById<Button>(R.id.submit)
+        submit.setOnClickListener {
+            val emailid = findViewById<EditText>(R.id.login_email)
+            val email = emailid.text.toString()
+            val number = findViewById<EditText>(R.id.phoneno)
+            val phone = number.text.toString()
+            if (isValidEmail(email) && isValidPhone(phone)) {
+                // Email is valid, submit the form
+                val toast = Toast.makeText(this, "Form submitted successfully", Toast.LENGTH_SHORT)
+                toast.show()
+            } else if(!isValidEmail(email)) {
+                // Email is invalid, show an error message
+                emailid.error = "Invalid email address"
+            } else {
+                number.error = "Invalid phone number"
+            }
+        }
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isValidPhone(phone: String): Boolean {
+        return phone.length == 10 && android.util.Patterns.PHONE.matcher(phone).matches()
+
     }
 }
